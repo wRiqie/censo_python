8fc466418191ed09c754b5a00a8f92a146315a4f
wRiqie <henriquegame512@outlook.com>
2023-11-04 11:23:19 -0300

    feat: :sparkles: graficos dinâmicos

--- home.py
+++ home.py
@@ -2,51 +2,53 @@ import plotly.express as px
 import streamlit as st
 from ler_excel import lerExcel
 
+excels = ["Censo 2022 - Crescimento Populacional - Brasil", 
+                "Censo 2022 - Pirâmide etária - Brasil",
+                "Censo 2022 - População por sexo - Brasil",
+                "Censo 2022 - Território - Brasil"]
+
 with st.sidebar:
         st.title("Arquivos")
         nome = st.radio(
                "Escolha uma tabela",
-               ["Censo 2022 - Crescimento Populacional - Brasil", 
-                "Censo 2022 - Pirâmide etária - Brasil",
-                "Censo 2022 - População por sexo - Brasil",
-                "Censo 2022 - Território - Brasil"],
+               excels,
         )
-        # st.text_input(label="Pesquisar", placeholder="Buscando por...")
-        # st.button("Pesquisar", type="primary", use_container_width=True)
 
-# UI
-# st.title("Em desenvolvimento")
-# st.subheader("Teste de desenvolvimento")
-
-
-# st.write(nome)
-# st.text("""A prática cotidiana prova que a expansão dos mercados mundiais 
-# ainda não demonstrou convincentemente que vai participar 
-# na mudança das formas de ação.""")
-
-# if(nome != "") :
-#         try:   
-# data_frame = lerExcel(nome)
-# st.dataframe(data_frame)
-        # except:
-        #        st.write("Nenhuma planilha encontrada :(")
-        
 st.title(nome)
 base_dados = lerExcel(nome)
 st.write(base_dados)
 
-# st.markdown(
-#         """
-# <style>
+graficos = {
+        excels[0]: {
+                "x": "Ano da pesquisa",
+                "y": "População(pessoas)",
+                "colors": {'População(pessoas)': '#008000'}
+        },
+        excels[1]: {
+                "x": "Grupo de idade",
+                "y": ["População feminina(pessoas)",
+                "População masculina(pessoas)"],
+                "colors": {'População feminina(pessoas)': '#008000', 'População masculina(pessoas)': '#FFFF00'}
+        },
+        excels[2]: {
+                "x": "Sexo",
+                "y": "População(pessoas)",
+                "colors": {'População(pessoas)': '#008000'}
+        },
+        excels[3]: {
+                "x": "Ano da pesquisa",
+                "y": ["Densidade demográfica(hab/km²)", "Área(km²)"],
+                "colors": {'Área(km²)': '#008000', 'Densidade demográfica(hab/km²)': '#FFFF00'}
+        },
+}
 
-# </style>
-#         """
-# )
+graficoSelecionado = graficos[nome]
 
 fig = px.bar(base_dados, 
-             x="Grupo de idade",
-             y=["População feminina(pessoas)",
-                "População masculina(pessoas)"],
-             title=f"{nome} - BarChart"
+             x=graficoSelecionado['x'],
+             y=graficoSelecionado['y'],
+             title=f"{nome} - BarChart",
+             color_discrete_map = graficoSelecionado['colors']
+        #      
         )
 st.plotly_chart(fig)
